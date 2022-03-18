e6d1656c0fa48714adad128eb665bae878df80a0
Jose Tiznado <jtiznado@sdsu.edu>
2022-03-17 17:24:28 -0700

    Adjusted sleep time between page scrolls

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def find_jobs():
 
     while True:
         driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        time.sleep(2)
+        time.sleep(3)
         new_height = driver.execute_script('return document.body.scrollHeight')
         if new_height == previous_height:
             break
